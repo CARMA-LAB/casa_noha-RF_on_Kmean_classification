4193dbe6ad68f272ed3715a3b61b43794d4cb413
CARMA-LAB <studioguida.a408@outlook.it>
2026-01-30 17:01:31 +0100

    Initial commit

---
+++ pointclouds/casa_noha-Kmeans_classification/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 11778757,
+    "boundingBox": {
+        "lx": 22.099700927734376,
+        "ly": -17.379037857055665,
+        "lz": 23.808,
+        "ux": 56.69570092773438,
+        "uy": 17.21696214294434,
+        "uz": 58.404
+    },
+    "tightBoundingBox": {
+        "lx": 22.099700927734376,
+        "ly": -17.379037857055665,
+        "lz": 23.808,
+        "ux": 56.69570092773438,
+        "uy": 13.856962142944337,
+        "uz": 50.496
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.2996101379394531,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
